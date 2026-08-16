3980e946cb5a38c4b7523c15b2b95896c3094955
hurstin <132925282+hurstin@users.noreply.github.com>
2026-08-16 15:53:19 +0100

    feat: add automatic admin seeder on startup

--- backend/src/users/users.module.ts
+++ backend/src/users/users.module.ts
@@ -3,11 +3,12 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './user.entity';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
+import { UsersSeederService } from './users-seeder.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([User])],
   controllers: [UsersController],
-  providers: [UsersService],
+  providers: [UsersService, UsersSeederService],
   exports: [TypeOrmModule, UsersService],
 })
 export class UsersModule {}
